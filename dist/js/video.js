259e5570364ab8a1c35d82ec8bc9786f844ca911
mxaba <samuel.mcebo@gmail.com>
2020-02-07 01:01:26 +0200

    second push

---
+++ dist/js/video.js
@@ -0,0 +1,26 @@
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
+
+
+var figure = $(".video");
+var vid = figure.find("video");
+
+[].forEach.call(figure, function (item,index) {
+    item.addEventListener('mouseover', hoverVideo.bind(item,index), false);
+    item.addEventListener('mouseout', hideVideo.bind(item,index), false);
+});
+
+function hoverVideo(index, e) {
+    vid[index].play(); 
+}
+
+function hideVideo(index, e) {
+    vid[index].pause(); 
+}
